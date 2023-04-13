b3ae5fc2dba1520d7ce7574b397ba1b7f7c4ca93
Lincoln Atkinson <LINCOLN.ATKINSON2478@STU.MTEC.EDU>
2023-04-12 23:22:27 -0600

    first commit

---
+++ jS.js
@@ -0,0 +1,81 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+storeItems.forEach(function(item){
+    if (item.inStock){
+        $('#list').append('<p class = "item">$'+item.price+'<span></span>'+item.name+'<span></span>'+item.details+'</p>');
+    }
+})
+
+const darkButton = document.getElementById('button')
+darkButton.addEventListener(`click`, function(){
+    $('div, body, html, h1, .item, #button').toggleClass('dark');
+    $('p').toggleClass('light');
+    $('#button p').toggleClass('light');
+})
